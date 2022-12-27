77af8b2d2db7de3339915ce87e687bf7fb5ca428
Marko <markofilipovicsd@gmail.com>
2022-12-27 21:53:24 +0100

    Osnova pocetne strane, header i navbar

---
+++ pocetna.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Početna</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+
+<section class="header">
+
+   <a class="logo" href="pocetna.php"><img src="slike/logo.PNG" height="74px" width="280px"></a></div>
+
+   <nav class="navbar">
+      <a href="pocetna.php">Početna</a>
+      <a href="o_nama.php">O nama</a>
+      <a href="aranzman.php">Aranžmani</a>
+      <a href="bukiranje.php">Bukirajte</a>
+      <a href="#kontakt">Kontakt</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
