9b1066c4cd0d06e915834c7d497de403bce40b6f
Marko <markofilipovicsd@gmail.com>
2022-12-29 23:10:56 +0100

    aranzman header

--- aranzman.php
+++ aranzman.php
@@ -1,16 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>aranžmani</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aranžmani</title>
 
-   <!-- swiper css link  -->
-   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+    <!-- swiper css link  -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
 
-   <!-- font awesome cdn link  -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-   <link rel="stylesheet" href="css/style.css">
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
 
 </head>
+
+<body>
+
+
+
+    <section class="header">
+
+
+        <a class="logo" href="pocetna.php"><img src="slike/logo.PNG" height="74px" width="280px"></a></div>
+
+        <nav class="navbar">
+            <a href="pocetna.php">Početna</a>
+            <a href="o_nama.php">O nama</a>
+            <a href="aranzman.php">Aranžmani</a>
+            <a href="bukiranje.php">Bukirajte</a>
+            <a href="#kontakt">Kontakt</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+    </section>
+
+
+
+    <div class="heading" style="background:url(slike/header-bg-2.png) no-repeat">
+        <h1>aranžmani</h1>
+    </div>
